1207ad2dd7b2e7229cad076addb18f27fde38f4b
AVIX <avix256@gmail.com>
2023-03-07 11:07:54 +0700

    first

---
+++ components/A-Autocomplete.vue
@@ -0,0 +1,84 @@
+<template>
+  <div style="width: 100%">
+    <v-autocomplete
+      v-if="filtered.length"
+      v-model="selected"
+      v-click-outside="selectValue"
+      :dense="dense"
+      :hide-details="hideDetails"
+      validate-on-blur
+      hide-no-data
+      :disabled="disabled"
+      :outlined="outlined"
+      :items="allItems"
+      :label="label"
+      :search-input.sync="searchValue"
+      @keydown.tab="selectValue"
+    ></v-autocomplete>
+    <!-- @update:search-input="searchInput" -->
+    <v-text-field
+      v-else
+      :dense="dense"
+      :hide-details="hideDetails"
+      :outlined="outlined"
+      :clearable="clearable"
+      :disabled="disabled"
+      :label="label"
+      :value="value"
+      @input="input"
+    ></v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: { type: String, default: '' },
+    label: { type: String, default: '' },
+    clearable: { type: Boolean, default: true },
+    outlined: { type: Boolean, default: false },
+    dense: { type: Boolean, default: false },
+    disabled: { type: Boolean, default: false },
+    hideDetails: { type: Boolean, default: false },
+    items: {
+      type: Array,
+      default: () => {
+        return []
+      },
+    },
+  },
+  data: () => ({
+    searchValue: '',
+    selected: '',
+    isSelecting: false,
+  }),
+  computed: {
+    filtered() {
+      return this.items.filter((x) => x)
+    },
+    allItems() {
+      if (this.value.length === 0 || this.isSelecting) return this.filtered
+      return [this.value].concat(this.filtered)
+    },
+  },
+  watch: {
+    searchValue(val) {
+      this.isSelecting = true
+      if (val !== null) this.$emit('input', val)
+      else this.selectValue()
+    },
+  },
+  methods: {
+    setSelecting() {
+      this.isSelecting = true
+    },
+    input(val) {
+      this.$emit('input', val || '')
+    },
+    selectValue() {
+      this.isSelecting = false
+      this.selected = this.value
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,487 @@
+<template>
+  <v-row justify="center" align="center">
+    <div class="fixed">
+      <v-switch v-model="outlined"></v-switch>
+      <v-switch v-model="signatures"></v-switch>
+      <v-switch v-model="labels"></v-switch>
+    </div>
+    <v-col>
+      <v-row justify="space-between">
+        <v-col cols="6">
+          <v-row>
+            <v-card-title>АКТ</v-card-title>
+            <v-col>
+              <v-text-field
+                :outlined="outlined"
+                dense
+                hide-details
+                :prefix="labels ? '' : '№'"
+                :label="labels ? 'Номер документа' : ''"
+                :items="[]"
+              />
+            </v-col>
+            <v-col>
+              <v-menu
+                v-model="dateMenu"
+                :close-on-content-click="false"
+                transition="scale-transition"
+                offset-y
+                min-width="auto"
+              >
+                <template #activator="{ on, attrs }">
+                  <v-text-field
+                    v-model="date"
+                    :outlined="outlined"
+                    label="Дата"
+                    hide-details
+                    dense
+                    append-icon="mdi-calendar"
+                    readonly
+                    v-bind="attrs"
+                    v-on="on"
+                  ></v-text-field>
+                </template>
+                <v-date-picker
+                  v-model="date"
+                  locale="ru"
+                  @input="dateMenu = false"
+                ></v-date-picker>
+              </v-menu>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <v-card-title
+              style="word-break: break-word !important"
+              class="pt-0 pb-4"
+              >О ПЕРЕДАЧЕ ТОВАРОВ И ТАРЫ ПРИ СМЕНЕ МАТЕРИАЛЬНО ОТВЕТСТВЕННОГО
+              ЛИЦА</v-card-title
+            >
+          </v-row>
+          <v-row
+            class="mx-0 mb-5"
+            :style="{ paddingBottom: outlined ? 0 : '10px' }"
+          >
+            <A-Autocomplete
+              v-model="organization"
+              :outlined="outlined"
+              hide-details
+              dense
+              label="Организация"
+              :items="organizations.map((organization) => organization.name)"
+            />
+          </v-row>
+          <v-row
+            class="mx-0 mb-5"
+            :style="{ paddingBottom: outlined ? 0 : '10px' }"
+          >
+            <A-Autocomplete
+              v-model="subdivision"
+              :outlined="outlined"
+              hide-details
+              dense
+              label="Подразделение"
+              :items="
+                organization
+                  ? organizations.filter((o) => o.name === organization)?.[0]
+                      ?.subdivisions
+                  : organizations.reduce((acc, cur) => {
+                      acc.push(...cur.subdivisions)
+                      return acc
+                    }, [])
+              "
+            />
+          </v-row>
+        </v-col>
+
+        <v-col cols="3"> </v-col>
+
+        <v-col cols="3">
+          <v-row
+            v-for="detail in documentDetails"
+            :key="detail.label"
+            class="ma-0 mb-5"
+            :style="{ paddingBottom: outlined ? 0 : '10px' }"
+          >
+            <A-Autocomplete
+              v-model="detail.value"
+              hide-details
+              :outlined="outlined"
+              :disabled="detail.disabled"
+              dense
+              :label="detail.label"
+            />
+          </v-row>
+        </v-col>
+      </v-row>
+
+      <v-data-table
+        :items="items"
+        :headers="headers"
+        :search="search"
+        :footer-props="{
+          showCurrentPage: true,
+        }"
+        group-by="type"
+        group-desc
+        dense
+      >
+        <template #header>
+          <thead class="v-data-table-header">
+            <tr>
+              <th></th>
+              <th colspan="2" class="text-center">Товар, тара</th>
+              <th colspan="2" class="text-center">Единица измерения</th>
+              <th colspan="5" class="px-0">
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="Поиск"
+                  single-line
+                  flat
+                  dense
+                  hide-details
+                ></v-text-field>
+              </th>
+            </tr>
+          </thead>
+        </template>
+
+        <template #[`group.header`]="{ group }">
+          <td></td>
+          <td colspan="2" class="font-weight-bold">{{ group }}</td>
+          <td v-for="h in headers.slice(3)" :key="h.name"></td>
+        </template>
+
+        <template #[`group.summary`]="groupProps">
+          <td></td>
+          <td class="text-right">Итого</td>
+          <td
+            v-for="h in headers.slice(2)"
+            :key="h.value"
+            :class="{
+              [`text-${h.align}`]: true,
+              'font-weight-bold': !h.summable,
+            }"
+          >
+            {{
+              h.summable
+                ? items
+                    .filter((p) => p.type === groupProps.group)
+                    .reduce((acc, cur) => acc + (cur[h.value] || 0), 0)
+                : 'X'
+            }}
+          </td>
+        </template>
+
+        <template #[`body.append`]>
+          <tr class="v-row-group__summary">
+            <td></td>
+            <td class="text-right">Всего по акту</td>
+            <td
+              v-for="h in headers.slice(2)"
+              :key="h.value"
+              :class="{
+                [`text-${h.align}`]: true,
+                'font-weight-bold': !h.summable,
+              }"
+            >
+              {{
+                h.summable
+                  ? items.reduce((acc, cur) => acc + (cur[h.value] || 0), 0)
+                  : 'X'
+              }}
+            </td>
+          </tr>
+        </template>
+
+        <template #item="{ item }">
+          <tr>
+            <td
+              v-for="h in headers"
+              :key="h.value"
+              :class="{ [`text-${h.align}`]: true, 'px-0': h.value !== 'n' }"
+            >
+              <template v-if="h.value === 'n'">{{
+                item.name ? item.n : ''
+              }}</template>
+
+              <v-icon
+                v-else-if="h.value === 'actions'"
+                small
+                @click="deleteItem(item)"
+              >
+                {{item.SKU ? 'mdi-delete' : ''}}
+              </v-icon>
+
+              <A-Autocomplete
+                v-else
+                v-model="products.filter((p) => p.SKU === item.SKU)[0][h.value]"
+                hide-details
+                dense
+                :clearable="false"
+                :items="
+                  h.value === 'total'
+                    ? [item.amount * item.price, item.weight * item.price]
+                    : []
+                "
+              />
+            </td>
+          </tr>
+        </template>
+      </v-data-table>
+      <v-col></v-col>
+      <template v-if="signatures">
+        <v-row v-for="personData in personsData" :key="personData.label">
+          <v-col class="py-0">
+            <v-subheader>{{ personData.label }}</v-subheader>
+          </v-col>
+
+          <v-col :class="{ 'py-0': true, 'pb-3': !outlined }"
+            ><A-Autocomplete
+              v-model="personData.position"
+              :outlined="outlined"
+              dense
+              hide-details
+              label="Должность"
+              :items="positions.map((p) => p.title)"
+          /></v-col>
+          <v-col :class="{ 'py-0': true, 'pb-3': !outlined }">
+            <A-Autocomplete
+              v-model="personData.fullname"
+              :outlined="outlined"
+              dense
+              hide-details
+              label="ФИО"
+              :items="persons.map((p) => p.fullname)"
+            />
+          </v-col>
+        </v-row>
+      </template>
+
+      <v-dialog v-else v-model="signaturesDialog" width="1177">
+        <template #activator="{ on, attrs }">
+          <v-btn color="primary" v-bind="attrs" v-on="on"> Подписи </v-btn>
+        </template>
+        <v-card>
+          <v-card-title>Подписи</v-card-title>
+          <v-card-text>
+            <v-row
+              v-for="personData in personsData"
+              :key="personData.label"
+              class="my-0"
+            >
+              <v-col class="py-0">
+                <v-subheader>{{ personData.label }}</v-subheader>
+              </v-col>
+
+              <v-col :class="{ 'py-0': true, 'pb-3': !outlined }"
+                ><A-Autocomplete
+                  v-model="personData.position"
+                  :outlined="outlined"
+                  dense
+                  hide-details
+                  label="Должность"
+                  :items="positions.map((p) => p.title)"
+              /></v-col>
+              <v-col :class="{ 'py-0': true, 'pb-3': !outlined }">
+                <A-Autocomplete
+                  v-model="personData.fullname"
+                  :outlined="outlined"
+                  dense
+                  hide-details
+                  label="ФИО"
+                  :items="persons.map((p) => p.fullname)"
+                />
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-dialog>
+
+      <v-row class="my-5 mx-0">
+        <v-spacer />
+        <v-btn> Экспорт&nbsp;<v-icon>mdi-export</v-icon> </v-btn>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data: () => ({
+    outlined: true,
+    signatures: true,
+    labels: true,
+    search: '',
+    date: '',
+    pageStart: 0,
+    pageStop: 0,
+    itemsLength: 0,
+    organization: '',
+    subdivision: '',
+    dateMenu: false,
+    signaturesDialog: false,
+    documentDetails: [
+      { label: 'ОКУД', value: '0330518', disabled: true },
+      { label: 'ОКПО', value: '' },
+      { label: 'Вид деятельности по ОКДП', value: '' },
+      { label: 'Вид операции', value: '' },
+    ],
+    personsData: [
+      { label: 'Сдал', position: '', fullname: '' },
+      { label: 'Принял', position: '', fullname: '' },
+      { label: 'Представитель администрации', position: '', fullname: '' },
+    ],
+    headers: [
+      { text: '№', value: 'n', align: 'end' },
+      { text: 'наименование', value: 'name' },
+      { text: 'код', value: 'SKU', align: 'end' },
+      { text: 'наименование', value: 'unitName', align: 'center' },
+      { text: 'код по ОКЕЙ', value: 'OKEI', align: 'right' },
+      { text: 'Масса', value: 'weight', align: 'end', summable: true },
+      { text: 'Количество', value: 'amount', align: 'end', summable: true },
+      { text: 'Цена, руб. коп.', value: 'price', align: 'end' },
+      {
+        text: 'Сумма, руб. коп.',
+        value: 'total',
+        align: 'end',
+        summable: true,
+      },
+      { text: 'Действия', value: 'actions', align: 'center' },
+    ],
+    organizations: [
+      { name: 'ООО "СОЛНЫШКО"', subdivisions: ['Столовая №1', 'Столовая №2'] },
+      {
+        name: 'ООО "Хлеб да соль"',
+        subdivisions: ['Столовая АлтГТУ', 'Столовая какая-то'],
+      },
+    ],
+    products: [
+      {
+        type: 'Товары',
+        name: 'Цыплята-бройлеры',
+        SKU: '1',
+        unitName: 'кг',
+        OKEI: '166',
+        weight: 20,
+        amount: 17,
+        price: 80,
+        total: 1600,
+      },
+      {
+        type: 'Товары',
+        name: 'Окорочка',
+        SKU: '2',
+        unitName: 'кг',
+        OKEI: '166',
+        weight: 5,
+        amount: 7,
+        price: 100,
+        total: 500,
+      },
+      {
+        type: 'Товары',
+        name: 'Голень цыплёнка',
+        SKU: '3',
+        unitName: 'кг',
+        OKEI: '166',
+        weight: 4,
+        amount: 4,
+        price: 120,
+        total: 480,
+      },
+      {
+        type: 'Товары',
+        name: 'Грудки цыплёнка',
+        SKU: '4',
+        unitName: 'кг',
+        OKEI: '166',
+        weight: 5,
+        amount: 5,
+        price: 120,
+        total: 600,
+      },
+      {
+        type: 'Товары',
+        name: 'Бедро цыплёнка',
+        SKU: '5',
+        unitName: 'кг',
+        OKEI: '166',
+        weight: 3,
+        amount: 3,
+        price: 120,
+        total: 360,
+      },
+      {
+        type: 'Тара',
+        name: 'Контейнер пластиковый',
+        SKU: '403',
+        unitName: 'шт',
+        OKEI: '796',
+        weight: 1,
+        amount: 5,
+        price: 50,
+        total: 250,
+      },
+      {
+        type: 'Товары',
+      },
+      {
+        type: 'Тара',
+      },
+    ],
+
+    productNames: [''],
+    positions: [
+      { title: 'Кладовщик', code: 1 },
+      { title: 'Заведующий производством', code: 2 },
+      { title: 'Управляющий', code: 3 },
+    ],
+    persons: [
+      { fullname: 'Замков В.И.', position: 1 },
+      { fullname: 'Деловой П.В.', position: 2 },
+      { fullname: 'Веселова М.Н.', position: 3 },
+    ],
+  }),
+  computed: {
+    items() {
+      const n = {}
+      return this.products.map((p) => {
+        n[p.type] = (n[p.type] || 0) + 1
+
+        return {
+          ...p,
+          n: n[p.type],
+        }
+      })
+    },
+  },
+  methods: {
+    addItem(type) {
+      this.products.push({ type })
+    },
+    deleteItem(item) {
+      this.products = this.products.filter((p) => p.SKU !== item.SKU)
+    },
+  },
+}
+</script>
+
+<style>
+td,
+th {
+  border: 1px solid lightgray !important;
+}
+
+.v-input__slot {
+  padding: 0 16px;
+}
+
+.fixed {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+</style>
